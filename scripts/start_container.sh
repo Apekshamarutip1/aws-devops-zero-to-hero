7db92eb58dc00f7cc77875e50a7a210719dbd47c
Apeksha M Prabhu <48539035+Apekshamarutip1@users.noreply.github.com>
2024-03-21 11:32:19 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,8 +3,8 @@ set -e
 
 # Pull the Docker image from Docker Hub
 # docker pull abhishekf5/simple-python-flask-app
-docker pull apeksha1100/simple-python-flask-app:latest
+docker pull apeksha1100/simple-python-flask-app
 
 # Run the Docker image as a container
 # docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
-docker run -d -p 5000:5000 apeksha1100/simple-python-flask-app:latest
+docker run -d -p 5000:5000 apeksha1100/simple-python-flask-app
